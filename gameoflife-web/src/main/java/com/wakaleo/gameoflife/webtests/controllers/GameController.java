e44f6343571b1484b01ed44a55d304e1193a6768
Ramesh Kasula <68405781+rameshaol1@users.noreply.github.com>
2020-11-30 19:10:57 -0700

    Edited the comment

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
@@ -17,7 +17,7 @@ import com.wakaleo.gameoflife.domain.Universe;
  *
  * GameController.java
  * Controller to navigate the buttons on the game pages
- * new commit on github-testing
+ * new commit on github-testing for Poll SCM
  */
 @Controller
 @RequestMapping("/game")
